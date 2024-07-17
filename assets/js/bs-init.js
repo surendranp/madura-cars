11d762a8c53a5414c36efe33abf04c1228d404e3
suren67 <surendranpandi02@gmail.com>
2024-07-17 15:43:19 +0530

    productsecondcommit

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -46,9 +46,68 @@ window.onload = function() {
 	// Reset the form fields when the page loads
 	document.getElementById("contactForm").reset();
 };
-// button 
-const Btn=document.querySelectorAll('.btn',add());
+// product ape display 
+// const products = [
+// 	{ id: 1, name: 'Piaggio APE 250cc',modal:"modal:2021" ,price: "price: ₹3,00,000.00", image: 'assets/images/cars/ape.png', description: 'Description of Product 1' },
+// 	{ id: 2, name: 'Piaggio APE 360cc', modal:"modal:2022", price: "price: ₹3,50,000.00", image: 'assets/images/cars/ape.png', description: 'Description of Product 2' },
+// 	{ id: 3, name: 'Piaggio APE 400cc', modal:"modal:2023", price: "price: ₹4,54,000.00", image: 'assets/images/cars/ape.png', description: 'Description of Product 3' }
+// ];
 
-add().addEventListener('click',()=>{
-	alert('hi this surendran');
-});
+// const productList = document.querySelector('.product-list');
+
+// products.forEach(product => {
+// 	const productCard = `
+//             <div class="col item" data-aos="fade-up" data-aos-delay="50">
+//              <img class="img-fluid" src="${product.image}" style="-webkit-transform: scaleX(-1);transform: scaleX(-1);" alt="${product.name}">
+
+//                     <div class="box b d-flex flex-row justify-content-around shadow rounded-2">
+//                     <div class="cont1 mt-2">
+//                         <h4 class="text-center text-uppercase "> ${product.name}</h4>
+//                         <p> ${product.modal}</p>
+//                     </div>
+//                     <div class="cont2 mt-3">
+//                         <a href="./product.html?id=${product.id}" class=" text-decoration-none bg-dark pt-2 pe-2 pb-2 ps-2 text-light ">View More</a>
+//                         <p class="mt-2">${product.price}</p>
+//                     </div>
+//                    </div>
+        
+//                 </div>
+	
+
+// 	`;
+// 	productList.innerHTML += productCard;
+// });
+
+// product_tata
+// const products_tata = [
+// 	{ id: 1, name: 'tata ace ',modal:"modal:2021" ,price: "price: ₹6,68,814.00", image: 'assets/images/cars/gold.png', description: 'Description of Product 1' },
+// 	{ id: 2, name: 'tata ace gold', modal:"modal:2022", price: "price: ₹3,50,000.00", image: 'assets/images/cars/ace_coverd_white-removebg-preview.png', description: 'Description of Product 2' },
+// 	{ id: 3, name: 'tata ace elt', modal:"modal:2023", price: "price: ₹4,54,000.00", image: 'assets/images/cars/ace_coverd_white-removebg-preview.png', description: 'Description of Product 3' }
+// ];
+
+// const productList_tata = document.querySelector('.product-list1');
+
+// products_tata.forEach(product => {
+	
+// 	const productCard = `
+	
+//             <div class="col item" data-aos="fade-up" data-aos-delay="50">
+//              <img class="img-fluid" src="${product.image}" style="-webkit-transform: scaleX(-1);transform: scaleX(-1);" alt="${product.name}">
+
+//                     <div class="box b d-flex flex-row justify-content-around shadow rounded-2">
+//                     <div class="cont1 mt-2">
+//                         <h4 class="text-center text-uppercase "> ${product.name}</h4>
+//                         <p> ${product.modal}</p>
+//                     </div>
+//                     <div class="cont2 mt-3">
+//                         <a href="./product_tata.html?id=${product.id}" class=" text-decoration-none bg-dark pt-2 pe-2 pb-2 ps-2 text-light ">View More</a>
+//                         <p class="mt-2">${product.price}</p>
+//                     </div>
+//                    </div>
+        
+//                 </div>
+	
+
+// 	`;
+// 	productList.innerHTML += productCard;
+// });
