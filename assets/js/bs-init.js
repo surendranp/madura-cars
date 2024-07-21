4b53542b38401d6b6e6b03bc56a8385b28a1bb9d
suren67 <surendranpandi02@gmail.com>
2024-07-21 22:55:56 +0530

    responisve finall

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -45,3 +45,67 @@ window.onload = function() {
 	// Reset the form fields when the page loads
 	document.getElementById("contactForm").reset();
 };
+
+// product_detail 
+document.addEventListener('DOMContentLoaded', function() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const name = urlParams.get('name');
+	const price = urlParams.get('price');
+	const img = urlParams.get('img');
+	const modal=urlParams.get('modal');
+	const fc=urlParams.get('fc');
+	const insurance=urlParams.get('insurance');
+	const owner=urlParams.get('owner');
+	const number=urlParams.get('number');
+
+
+
+
+	const thumb1 = urlParams.get('thumb1');
+	const thumb2 = urlParams.get('thumb2');
+	const thumb3 = urlParams.get('thumb3');
+
+	document.getElementById('productName').innerText = name;
+	document.getElementById('productmodal').innerHTML=modal;
+	document.getElementById('productnumber').innerHTML=number;
+	document.getElementById('productowner').innerHTML=owner;
+	document.getElementById('productfc').innerHTML=fc;
+	document.getElementById('productinsurance').innerHTML=insurance;
+	document.getElementById('productPrice').innerText = `${price}`;
+
+
+
+
+
+	document.getElementById('productImage').src = img;
+	document.getElementById('thumb1').src = thumb1;
+	document.getElementById('thumb2').src = thumb2;
+	document.getElementById('thumb3').src = thumb3;
+	document.getElementById('thumb1').classList.add('selected-thumbnail'); // Highlight the first thumbnail by default
+
+});
+// function changeImage(thumbnail) {
+//     document.getElementById('productImage').src = thumbnail.src;
+//     const thumbnails = document.querySelectorAll('.thumbnail-img');
+//     thumbnails.forEach(img => img.classList.remove('selected-thumbnail'));
+//     thumbnail.classList.add('selected-thumbnail');
+// }
+function changeImage(thumbnail) {
+	const productImage = document.getElementById('productImage');
+	
+	productImage.classList.add('fade-left'); // Start fade left animation
+
+	setTimeout(() => {
+		productImage.src = thumbnail.src;
+		productImage.classList.remove('fade-left'); // Remove fade left animation
+		productImage.classList.add('fade-in'); // Start fade in animation
+	}, 500);
+
+	setTimeout(() => {
+		productImage.classList.remove('fade-in'); // Remove fade in animation
+	}, 1000);
+
+	const thumbnails = document.querySelectorAll('.thumbnail-img');
+	thumbnails.forEach(img => img.classList.remove('selected-thumbnail'));
+	thumbnail.classList.add('selected-thumbnail');
+}
